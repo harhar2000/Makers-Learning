056cb28ca4deb790237775097c188318c594b9a5
Harry <hmsh2000@gmail.com>
2024-09-10 19:59:07 +0100

    die

--- OOP/roll_the_dice.py
+++ OOP/roll_the_dice.py
@@ -1,6 +1,27 @@
-# Run program
-# Roll the dice? (y/n): 
-# take input. if letter - Invalid Choice!
-#             if num:
-# get two random numbres (5, 2) or (1, 4)
+#Loop
+    # Ask: roll the dice?
+    # If user enters y
+    #   Generate two random numbers
+    #   Print them
+    # If user enters n
+    #   Print thank you message
+    #   Terminate
+    # Else:
+    #   Print invalid choice
+
+import random
+
+while True:
+    choice = input("Roll the dice? (y/n): ").lower()
+    if choice == "n":
+        print("Thanks for playing!")
+        break
+    elif choice == "y":
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f"You rolled: {die1} and {die2}")
+    else:
+        print("Invalid choice!")
+
+
 
